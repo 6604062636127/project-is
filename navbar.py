945e68b784d80b20a974c26dd471683a195bcf91
Zitihayath Worawatthanachai <153045749+6604062636127@users.noreply.github.com>
2025-03-16 01:57:55 +0700

    Update navbar.py

--- navbar.py
+++ navbar.py
@@ -5,11 +5,13 @@ def navbar():
     st.markdown("""
         <style>
             .navbar {
-                background-color: #333;
+                background-color: #4CAF50; /* สีพื้นหลัง */
                 overflow: hidden;
                 display: flex;
                 justify-content: center;
                 padding: 10px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* เงา */
+                border-radius: 5px; /* มุมมน */
             }
             .navbar a {
                 color: white;
@@ -17,10 +19,14 @@ def navbar():
                 padding: 14px 20px;
                 font-size: 18px;
                 font-weight: bold;
+                transition: background-color 0.3s; /* การเปลี่ยนสีพื้นหลัง */
             }
             .navbar a:hover {
-                background-color: #575757;
+                background-color: #45a049; /* สีพื้นหลังเมื่อ hover */
                 border-radius: 5px;
+            }
+            .navbar a.active {
+                background-color: #3e8e41; /* สีพื้นหลังสำหรับลิงก์ที่ใช้งานอยู่ */
             }
         </style>
     """, unsafe_allow_html=True)
@@ -28,12 +34,10 @@ def navbar():
     # สร้าง Navbar
     st.markdown("""
         <div class="navbar">
-            <a href="/?page=Home" target="_self">Home</a>
+            <a href="/?page=Home" target="_self" class="active">Home</a>
             <a href="/?page=Doc1" target="_self">Document ML</a>
             <a href="/?page=Doc2" target="_self">Demo Machine Learning</a>
             <a href="/?page=ML" target="_self">Document NN</a>
-            <a href="/?page=NN" target="_self">Nueral Network</a>
+            <a href="/?page=NN" target="_self">Neural Network</a>
         </div>
     """, unsafe_allow_html=True)
-
-    
