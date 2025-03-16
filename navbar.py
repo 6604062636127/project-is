125841bece7af4e01d737d32207f65653e5bcedc
Zitihayath Worawatthanachai <153045749+6604062636127@users.noreply.github.com>
2025-03-16 11:07:22 +0700

    Update navbar.py

--- navbar.py
+++ navbar.py
@@ -48,7 +48,7 @@ def navbar():
 
     /* ลิงก์ที่กำลังใช้งาน */
     .navbar a.active {
-        background: #4CAF50;
+        background: #3B82F6;
         font-weight: bold;
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
     }
